9f0d2f16cb63a0629fdda16e168bda96522f089c
StilCyber <karnozavr666@ya.ru>
2023-03-14 22:03:05 +0300

    lesson 29

--- src/index.js
+++ src/index.js
@@ -3,32 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state';
 
 
-let posts = [
-   {message: 'Hi, how are you', likesCount: 2},
-   {message: 'It is my first post', likesCount: 5},
-]
 
-let dialogs = [
-   {id: 1, name: 'Dimych'},
-   {id: 2, name: 'Roman'},
-   {id: 3, name: 'Sveta'},
-   {id: 4, name: 'Valera'},
-   {id: 5, name: 'Andrew'},
-   {id: 6, name: 'Victor'},
-]
-let messages = [
-   {id: 1, message: 'Hi'},
-   {id: 2, message: 'How is your it-kamasutra?'},
-   {id: 3, message: 'Yo'},
-]
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App state={state}/>
   </React.StrictMode>
 
 );
---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+let state = {
+
+   profilePage: {
+      posts: [
+         {message: 'Hi, how are you', likesCount: 2},
+         {message: 'It is my first post', likesCount: 5},
+      ],
+   
+   },
+
+   dialogsPage: {
+      dialogs: [
+         {id: 1, name: 'Dimych'},
+         {id: 2, name: 'Roman'},
+         {id: 3, name: 'Sveta'},
+         {id: 4, name: 'Valera'},
+         {id: 5, name: 'Andrew'},
+         {id: 6, name: 'Victor'},
+      ],
+      messages: [
+         {id: 1, message: 'Hi'},
+         {id: 2, message: 'How is your it-kamasutra?'},
+         {id: 3, message: 'Yo'},
+      ],
+   },
+
+}
+
+export default state;
